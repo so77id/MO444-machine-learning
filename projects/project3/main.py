ba12877c40b8be2e215da18fea45c5daa0e8bef7
ecclesiedei <ecclesiedei@gmail.com>
2017-10-27 17:51:43 -0200

    baseline k=3

--- projects/project3/main.py
+++ projects/project3/main.py
@@ -1,10 +1,13 @@
 import sys
 import argparse
 import numpy as np
-
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 from utils.load_dataset import load_dataset
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
 
-def main(argv):
+def baseline(argv):
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input_file', help='input file', required=True)
@@ -14,10 +17,21 @@ def main(argv):
 
 
     descriptors = load_dataset(ARGS.input_file)
+    # trying k-means
+    k_means = KMeans(n_clusters=3, random_state=0)
+    k_means.fit(descriptors)
 
+    predictions = k_means.predict(descriptors)
 
+    pca = PCA(n_components=2)
+    pca.fit(descriptors)
+    X_reduced = pca.transform(descriptors)
+
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=predictions,
+           cmap='RdYlBu')
+    plt.show()
 
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(baseline(sys.argv[1:]))

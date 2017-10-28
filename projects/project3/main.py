7101768656e748df4ee1f0a68b287b48a43bdc15
ecclesiedei <ecclesiedei@gmail.com>
2017-10-28 11:37:45 -0200

    modificacao para plot 3d

--- projects/project3/main.py
+++ projects/project3/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from utils.load_dataset import load_dataset
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 
@@ -21,17 +22,30 @@ def baseline(argv):
     # trying k-means
 
     print("[Baseline] Clustering")
-    k_means = KMeans(n_clusters=4, random_state=0)
+    k_means = KMeans(n_clusters=7, random_state=0)
     k_means.fit(descriptors)
 
     predictions = k_means.predict(descriptors)
 
-    pca = PCA(n_components=2)
+    """pca = PCA(n_components=2)
     pca.fit(descriptors)
     X_reduced = pca.transform(descriptors)
     print("[Baseline] Plotting")
     plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=predictions,
            cmap='RdYlBu')
+    plt.show()"""
+
+    # 3d projection
+    pca = PCA(n_components=3)
+    pca.fit(descriptors)
+    X_reduced = pca.transform(descriptors)
+    print("[Baseline] Plotting")
+    fig = plt.figure(1, figsize=(8,8))
+    plt.clf()
+    ax = Axes3D(fig, rect=[0, 0, 1, 1], elev=8, azim=200)
+    plt.cla()
+
+    ax.scatter(X_reduced[:, 2], X_reduced[:, 0], X_reduced[:, 1], c=predictions)
     plt.show()
 
 
